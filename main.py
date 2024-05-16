771e636dfc338990c5451663f140bc547b494b4d
Thiago S. Brasil <thiagosbrasil@ua.pt>
2024-05-16 22:37:42 +0100

    test: :green_heart: Test pipeline

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Deployed"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
