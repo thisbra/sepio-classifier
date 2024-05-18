b63ec785f33f09f17225ba733a8cd96e63ed0545
Thiago S. Brasil <thiagosbrasil@ua.pt>
2024-05-19 00:47:38 +0100

    refactor: :coffin: Removed MacAddress model

--- src/models/MacAddress.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-from pydantic import BaseModel
-from fastapi import Query
-
-# MacAddress is a string of 12 hexadecimals between 0-9 and A-F 
-class MacAddress(BaseModel):
-    mac_address: Annotated[str, Query(..., min_length=12, max_length=12)]
-    
