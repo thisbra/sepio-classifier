073d19fd9b83cb762befbad220ffa38525666faf
Thiago S. Brasil <thiagosbrasil@ua.pt>
2024-05-21 22:25:23 +0100

    refactor: :mute: Removed print statements

--- src/utils/functions/AddressIsInRange.py
+++ src/utils/functions/AddressIsInRange.py
@@ -2,7 +2,6 @@ from .HexidecimalToDecimal import HexadecimalToDecimal
 
 # range format: '000000-FFFFFF'
 def AddressIsInRange(address: str, range: str) -> bool:
-    print(f"Checking if {address} is in range {range}")
     if address is None or range is None:
         return False
     address = HexadecimalToDecimal(address)
